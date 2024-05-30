604bbbe816cb8ad66818dd8d65d50bee1468dd6d
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-05-30 17:59:13 +0200

    little fixes on login page

--- inertia/pages/login.tsx
+++ inertia/pages/login.tsx
@@ -1,5 +1,5 @@
 import { Field, Label, Input, Fieldset, Button } from '@headlessui/react'
-import { Head } from '@inertiajs/react'
+import { Head, router } from '@inertiajs/react'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 
@@ -18,6 +18,7 @@ export default function Login() {
   const onSubmit = async (data: FormData) => {
     try {
       const response = await axios.post('/api/auth/login', data)
+      router.visit('/')
       console.log(response)
     } catch (error) {
       console.log(error.message)
@@ -31,7 +32,7 @@ export default function Login() {
         <span
           className={`h-full w-full hidden lg:block bg-cover bg-center bg-[url(public/img/login_page_image.jpg)]`}
         ></span>
-        <div className="flex flex-col justify-center space-y-10 h-full w-full sm:p-16 md:p-24 lg:p-16">
+        <div className="flex flex-col space-y-10 h-full w-full sm:p-16 md:p-24 lg:p-16">
           <h1 className="text-3xl text-center font-semibold text-primary">PAWWWS.</h1>
           <form onSubmit={handleSubmit(onSubmit)}>
             <h2 className="text-2xl text-center font-medium">Connexion</h2>

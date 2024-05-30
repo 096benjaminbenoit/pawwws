af46b7766f344160521aba7f69d16a27650a5f1f
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-05-30 10:21:24 +0200

    add custom colors tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./inertia/**/*.{js,ts,jsx,tsx,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Jost', 'ui-sans-serif', 'system-ui'],
+    },
+    extend: {
+      colors: {
+        primary: '#6366F1',
+        dark: '#1C2536',
+        error: '#f82041',
+        success: '#60cb6a',
+        lightbase: '#F6F8FB',
+      },
+    },
   },
   plugins: [],
 }

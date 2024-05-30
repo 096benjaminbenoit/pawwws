33688f15b097f081adc0a73ab988d49a54740818
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-05-30 08:55:19 +0200

    api routes ok

--- start/routes.ts
+++ start/routes.ts
@@ -1,12 +1,31 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
+/* eslint-disable prettier/prettier */
 import router from '@adonisjs/core/services/router'
-router.on('/inertia').renderInertia('home', { version: 6 })
+import { middleware } from './kernel.js'
+const AuthController = () => import('#controllers/auth_controller')
+const OrganizationsController = () => import('#controllers/organizations_controller')
+const RegistrationController = () => import('#controllers/registration_controller')
 
+router.on('/').renderInertia('home', { version: 6 })
+
+router
+  .group(() => {
+    router
+      .post('/organizations/register', [OrganizationsController, 'registerOrganization'])
+      .use(middleware.guest())
+    router
+      .post('/users/register', [RegistrationController, 'registerUser'])
+      .use(middleware.guest())
+    router
+      .post('/send-invitation', [RegistrationController, 'sendInvitation'])
+      .use(middleware.auth())
+    router
+      .post('/users/register/:token', [RegistrationController, 'registerUserByInvitation'])
+      .use(middleware.guest())
+    router
+      .post('/auth/login', [AuthController, 'loginUser'])
+      .use(middleware.guest())
+    router
+      .post('/auth/logout', [AuthController, 'logoutUser'])
+      .use(middleware.auth())
+  })
+  .prefix('/api')

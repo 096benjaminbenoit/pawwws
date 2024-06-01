25d13c200f5ce05df874ba89b729f276240402e7
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-06-01 09:16:37 +0200

    search method and all animals method with pagination ok

--- app/controllers/animals_controller.ts
+++ app/controllers/animals_controller.ts
@@ -4,8 +4,44 @@ import type { HttpContext } from '@adonisjs/core/http'
 import { UserRole } from '../enums/user_roles.js'
 
 export default class AnimalsController {
-  index({ inertia }: HttpContext) {
+  index({ inertia, auth, response }: HttpContext) {
+    if (!auth.user) return response.status(401).send({ message: 'You must be connected' })
+
     return inertia.render('dashboard/animals')
+  }
+
+  async getAnimals({ auth, response, request }: HttpContext) {
+    if (!auth.user) return response.status(401).send({ message: 'You must be loggedin' })
+
+    const page = request.input('page', 1)
+    const limit = 6
+
+    try {
+      const animals = await Animal.query()
+        .where('organization_id', auth.user.organizationId)
+        .paginate(page, limit)
+
+      if (!animals) return response.status(400).send({ messag: 'No animals found' })
+
+      return response.status(200).send(animals)
+    } catch (error) {
+      return response.status(500).send(error.message)
+    }
+  }
+
+  async search({ auth, response, request }: HttpContext) {
+    if (!auth.user) return response.status(401).send({ message: 'You must be loggedin' })
+    const query = request.input('query')
+
+    try {
+      const results = await Animal.query()
+        .whereILike('name', `%${query}%`)
+        .orWhereILike('identification_number', `%${query}%`)
+
+      return response.status(200).send({ results })
+    } catch (error) {
+      return response.status(500).send(error.message)
+    }
   }
 
   show({ inertia }: HttpContext) {

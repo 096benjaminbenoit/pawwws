e60bb7e73d9e5a86ed24beadd23514fc2c68346a
096benjaminbenoit <096benjaminbenoit@gmail.com>
2024-06-01 08:36:43 +0200

    add animal type

---
+++ inertia/types/animal.ts
@@ -0,0 +1,19 @@
+export type Animal = {
+  id: number
+  identificationNumber: string
+  name: string
+  color: string
+  birthDate: Date
+  arivalDate: string
+  isSterilized: boolean
+  sex: string
+  species: string
+  race: string
+  comment: string
+  isAdopted: string
+  adoptionDate: string
+  adoptiveFamilyId: number
+  isHosted: string
+  hostFamilyId: number
+  hostingDate: string
+}
